a39467a50418641fe6afdbbd6d3be71fb77cd446
hahamidi <9990099mary>
2023-12-18 07:31:54 +0000

    First LLM class added

--- llm/example.py
+++ llm/example.py
@@ -2,9 +2,18 @@ from typing import List
 
 import torch
 from torch import nn
+from angle_emb import AnglE, Prompts
+
+class LLM(nn.Module):
+
+    def __init__(self, model_name: str, pooling_type: str,prompt : str):
+        super(LLM, self).__init__()
+
+        self.angle_model = AnglE.from_pretrained(model_name, pooling_strategy=pooling_type).cuda()
+        self.angle_model.set_prompt(prompt=prompt)
 
 
-class ExampleLLM(nn.Module):
+
     def forward(self, products: List[str]) -> torch.Tensor:
         """
         Arguments:
@@ -12,5 +21,11 @@ class ExampleLLM(nn.Module):
 
         Returns:
             output: Tensor, shape ``[B, D]``.
+
+            
         """
-        return torch.randn(len(products), 1024)
+
+        input_data = [{'text': text} for text in products]
+        tensor_output = self.angle_model.encode(input_data, to_numpy=True)
+        return torch.tensor(tensor_output)
+  
