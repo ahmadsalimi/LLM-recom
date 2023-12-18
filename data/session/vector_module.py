d3a2d106609781f62bca2e3183dc55f35e54fb79
ahmadsalimi <ahsa9978@gmail.com>
2023-12-18 17:25:10 -0500

    Auto-split

--- data/session/vector_module.py
+++ data/session/vector_module.py
@@ -1,7 +1,7 @@
 from typing import Optional, Union, Tuple, List
 
 from pytorch_lightning import LightningDataModule
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
 from data.product.io.io import VectorIO
 from data.session.vector_dataset import SessionVectorDataset
@@ -11,13 +11,15 @@ class SessionVectorDataModule(LightningDataModule):
 
     def __init__(self, batch_size: int,
                  sessions_file: Union[str, Tuple[str, str]],
+                 test_sessions_file: Union[str, Tuple[str, str]],
                  num_workers: int = 0,
                  vector_io: Optional[VectorIO] = None,
                  include_locale: Optional[List[str]] = None):
         super().__init__()
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.sessions_file = sessions_file
+        self.train_sessions_file = sessions_file
+        self.test_sessions_file = test_sessions_file
         self.vector_io = vector_io
         self.include_locale = include_locale
         self.train_dataset = None
@@ -27,13 +29,14 @@ class SessionVectorDataModule(LightningDataModule):
     def setup(self, stage: str = None) -> None:
         if stage == 'fit':
             self.vector_io.initialize_read()
-            self.train_dataset = SessionVectorDataset(self.sessions_file, self.vector_io,
-                                                      include_locale=self.include_locale)
-            self.val_dataset = SessionVectorDataset(self.sessions_file, self.vector_io,
-                                                    include_locale=self.include_locale)
+            train_dataset = SessionVectorDataset(self.train_sessions_file, self.vector_io,
+                                                 include_locale=self.include_locale)
+            self.train_dataset, self.val_dataset = random_split(train_dataset, [int(len(train_dataset) * 0.9),
+                                                                                len(train_dataset) -
+                                                                                int(len(train_dataset) * 0.9)])
         elif stage == 'test':
             self.vector_io.initialize_read()
-            self.test_dataset = SessionVectorDataset(self.sessions_file, self.vector_io,
+            self.test_dataset = SessionVectorDataset(self.train_sessions_file, self.vector_io,
                                                      include_locale=self.include_locale)
         else:
             raise ValueError(f'Unsupported stage: {stage}')
