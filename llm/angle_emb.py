48721a180d3b9a0230dd7fd916a15f4eced32dff
hahamidi <9990099mary>
2023-12-20 02:51:58 +0000

    llama embedding model added

--- llm/angle_emb.py
+++ llm/angle_emb.py
@@ -6,11 +6,16 @@ from angle_emb import AnglE, Prompts
 
 class LLM(nn.Module):
 
-    def __init__(self, model_name: str, pooling_type: str,prompt : str):
+    def __init__(self, model_name: str, pretrained_lora_path:str,pooling_type: str,prompt : str):
         super(LLM, self).__init__()
 
-        self.angle_model = AnglE.from_pretrained(model_name, pooling_strategy=pooling_type).cuda()
-        self.angle_model.set_prompt(prompt=prompt)
+        if model_name.split("/")[-1] == "UAE-Large-V1":
+            self.angle_model = AnglE.from_pretrained(model_name, pooling_strategy=pooling_type).cuda()
+            self.angle_model.set_prompt(prompt=prompt)
+        if model_name.split("/")[-1] == "Llama-2-7b-hf":
+            self.angle_model = AnglE.from_pretrained(model_name,pretrained_lora_path = pretrained_lora_path).cuda()
+            self.angle_model.set_prompt(prompt=prompt)
+
 
 
 
@@ -27,7 +32,7 @@ class LLM(nn.Module):
 
         input_data = [{'text': text} for text in products]
         tensor_output = self.angle_model.encode(input_data, to_numpy=False)
-        return tensor_output
+        return tensor_output.float()
 
 class BERT(nn.Module):
 
